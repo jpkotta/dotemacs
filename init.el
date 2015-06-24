581e461a41c4b9a6ac580aafadfbb7b447638bba
Jonathan Kotta <JK72405>
2015-06-24 14:21:11 -0500

    remove advice on completion--file-name-table
    it caused normal filename completion to fail
    i'm not sure it was even necessary

--- init.el
+++ init.el
@@ -888,17 +888,6 @@ The numbers are formatted according to the FORMAT string."
 (setq ido-auto-merge-work-directories-length -1)
 (setq ido-use-virtual-buffers t)
 
-;; from http://stackoverflow.com/a/1732081/245173
-(defadvice completion--file-name-table
-  (after ignoring-backups-f-n-completion activate)
-  "Filter out results when they match `completion-ignored-extensions'."
-  (let ((res ad-return-value))
-    (if (and (listp res)
-           (stringp (car res))
-           (cdr res)) ;; length > 1, don't ignore sole match
-        (setq ad-return-value
-              (completion-pcm--filename-try-filter res)))))
-
 (defun ido-initiate-auto-merge-this-buffer ()
   "Calls `ido-initiate-auto-merge' on the current buffer.
 
