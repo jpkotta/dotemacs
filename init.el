273f634b2a1c0c79249711b4fff86ae719e9e16e
Jonathan Kotta <JK72405>
2015-11-17 15:49:14 -0600

    tweak find-alternative-file-with-sudo

--- init.el
+++ init.el
@@ -1072,25 +1072,24 @@ This function is suitable to add to `find-file-hook'."
   (when (string-equal
          (file-remote-p (or buffer-file-name default-directory) 'user)
          "root")
-    (let* ((warning "WARNING: EDITING FILE AS ROOT!")
-           (space (+ 6 (- (window-width) (length warning))))
-           (bracket (make-string (/ space 2) ?-))
-           (warning (concat bracket warning bracket)))
-      (setq header-line-format
-            (propertize  warning 'face 'find-file-root-header-face)))))
+    (setq header-line-format
+          (propertize "--- WARNING: EDITING FILE AS ROOT! %-"
+                      'face 'find-file-root-header-face))))
 
 (defun find-alternative-file-with-sudo ()
   (interactive)
   (let ((bname (expand-file-name (or buffer-file-name
-                                     default-directory)))
-        (pt (point)))
+                                    default-directory)))
+        (pt (point))
+        (ws (window-start)))
     (setq bname (or (file-remote-p bname 'localname)
-                    (concat "/sudo::" bname)))
+                   (concat "/sudo::" bname)))
     (cl-letf (((symbol-function 'server-buffer-done)
                (lambda (buffer &optional for-killing)
                  nil)))
       (find-alternate-file bname))
-    (goto-char pt)))
+    (goto-char pt)
+    (set-window-start (selected-window) ws)))
 
 ;; normally this is bound to find-file-read-only
 ;; use M-x read-only-mode instead
