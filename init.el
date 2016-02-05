116c2b720ea21d2858daa8cc019d71d6749f2197
Jonathan Kotta <jpkotta@gmail.com>
2016-02-04 22:14:27 -0600

    replace align-regexp-repeated with align-regexp-{space,comma}

--- init.el
+++ init.el
@@ -3460,25 +3460,17 @@ of text."
               "Scroll to the left."
               (scroll-right (window-hscroll))))
 
-(defun align-regexp-repeated (start stop regexp)
-  "Like align-regexp, but repeated for multiple columns. See
-http://www.emacswiki.org/emacs/AlignCommands"
-  (interactive "r\nsAlign regexp: ")
-  (let ((spacing 1)
-        (old-buffer-size (buffer-size)))
-    ;; If our align regexp is just spaces, then we don't need any
-    ;; extra spacing.
-    (when (string-match regexp " ")
-      (setq spacing 0))
-    (align-regexp start stop
-                  ;; add space at beginning of regexp
-                  (concat "\\([[:space:]]*\\)" regexp)
-                  1 spacing t)
-    ;; modify stop because align-regexp will add/remove characters
-    (align-regexp start (+ stop (- (buffer-size) old-buffer-size))
-                  ;; add space at end of regexp
-                  (concat regexp "\\([[:space:]]*\\)")
-                  1 spacing t)))
+(defun align-regexp-space (beginning end)
+  "Align columns using space as a delimiter."
+  (interactive "*r")
+  (align-regexp beginning end
+                "\\(\\s-*\\)\\s-" 1 0 t))
+
+(defun align-regexp-comma (beginning end)
+  "Align columns using comma as a delimiter."
+  (interactive "*r")
+  (align-regexp beginning end
+                ",\\(\\s-*\\)" 1 1 t))
 
 ;; see also fill-individual-paragraphs
 
