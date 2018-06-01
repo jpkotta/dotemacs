edb536cebd4470902a419de9410054a685eac45c
Jonathan Kotta <jpkotta@gmail.com>
2018-06-01 14:59:10 -0500

    add minimal init script

---
+++ emacs-minimal.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+exec emacs -q -l $HOME/.emacs.d/lisp/minimal-init.el
---
+++ lisp/minimal-init.el
@@ -0,0 +1,26 @@
+
+(setq package-user-dir (expand-file-name
+                        (format "elpa-test")
+                        user-emacs-directory))
+
+(package-initialize)
+(setq package-archives '(("melpa-stable" . "http://stable.melpa.org/packages/")
+                         ("melpa" . "http://melpa.org/packages/")
+                         ("gnu" . "http://elpa.gnu.org/packages/")))
+(setq package-archive-priorities '(("melpa-stable" . 20)
+                                   ("gnu" . 10)
+                                   ("melpa" . 0)))
+
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+(setq use-package-always-ensure t
+      use-package-enable-imenu-support t)
+
+(when init-file-debug ;; --debug-init
+  (setq use-package-verbose 'debug
+        use-package-debug t
+        use-package-minimum-reported-time 0))
+
+(use-package no-littering)
