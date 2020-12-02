5e3cc2d0c95c18e633615a8226cdf657b5ee0b55
Jonathan Kotta <jpkotta@gmail.com>
2020-12-02 14:07:44 -0600

    don't use melpa stable

--- early-init.el
+++ early-init.el
@@ -53,7 +53,7 @@ Only defcustoms usually have a `standard-value'."
                         (format "elpa-%d" emacs-major-version)
                         user-emacs-directory))
 
-(setq package-archives '(("melpa-stable" . "http://stable.melpa.org/packages/")
+(setq package-archives '(;;("melpa-stable" . "http://stable.melpa.org/packages/")
                          ("melpa" . "http://melpa.org/packages/")
                          ("gnu" . "http://elpa.gnu.org/packages/")
                          ("onpa" . "https://olanilsson.bitbucket.io/packages/")))
